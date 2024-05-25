0f0c582e0188b336d3846f3bf5aa762d3932401b
Deathwing <p.kittichet@gmail.com>
2024-05-25 19:04:16 +0700

    test set chanel byname

--- server/app.js
+++ server/app.js
@@ -28,11 +28,20 @@ const fileFilter = (req, file, cb) => {
     }
 };
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  next();
-});
+app.use(
+    multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    );
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 
 app.use(cors({
     origin: true
--- server/controllers/exampleController.js
+++ server/controllers/exampleController.js
@@ -4,17 +4,17 @@ const io = require("../socket");
 
 exports.testMessage  =  (req, res) => {
     // console.log("Socket Id:", io.getIO().id);
-    const notify = { data: req.body };
-    console.log(req.body.username);
-    console.log(req.body.message);
-    console.log(req.body.time);
+    // console.log(req.body.username);
+    // console.log(req.body.message);
+    // console.log(req.body.time);
     const jsondata = {
         username:req.body.username,
         message:req.body.message,
         time:req.body.time,
     }
     // send notification to all connected clients
-    io.getIO().emit('chat:message', jsondata);
+    // io.getIO().emit('chat:message', jsondata);
+    io.getIO().emit('chat:message-'+req.body.username, jsondata);
     return res.status(200).json(jsondata);
     // res.send(notify);
 }
