04ea4b2c5de799e56e56f2f70c6a7c1043f3774c
Deathwing <p.kittichet@gmail.com>
2024-07-13 18:13:08 +0700

    move nextjs to client and fix admin get customer list

--- client/app/admin/page.js
+++ client/app/admin/page.js
@@ -4,7 +4,7 @@ import React, { useEffect, useState, useRef  } from 'react';
 import useSocket from '../service/useSocket';
 import { useRouter } from 'next/navigation';
 import { converstUTC } from '../service/utility';
-import { getUserEmail, getToken, logout } from "../service/authorize";
+import { getUserEmail, getPermission, getToken, logout } from "../service/authorize";
 
 export default function Page() {
 	const apiUrl = process.env.NEXT_PUBLIC_API_URL;
@@ -15,17 +15,27 @@ export default function Page() {
 	const [loading, setLoading] = useState(true);
 	const [messages, setMessages] = useState([]);
 	const [message, setMessage] = useState('');
+	const [socketId, setSocketId] = useState('');
+	const [callEmail, setCallEmail] = useState('');
     const [customerList, setcustomerList] = useState([]);
 	const router = useRouter();
 	const endOfPageRef = useRef(null);
 
 	useEffect(() => {
 		if (socket) {
+			if(getPermission()!="admin"){
+				logout();
+				router.push('/');
+				return false;
+			}
+	
 			const token = getToken();
 			const email = getUserEmail();
 			setToken(token);
 			setEmail(email);
-			fetch(apiUrl+"chat/previous", {
+
+			// get user list from DB
+			fetch("http://localhost:8081/chat/get_user_list", {
 				method: "GET",
 				headers: {
 					Authorization: `Bearer ${token}`
@@ -34,26 +44,11 @@ export default function Page() {
 				redirect: 'follow', // manual, *follow, error
 				referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
 			})
-				.then(response => response.json())
-				.then(data => {
-					try {
-						if (data.chatId == "" || data.chatId == null) {
-							logout();
-							router.push('/');
-						} else {
-							setChatId(data.chatId);
-							const allmsg = data.chatMessage;
-							for (const key in allmsg) {
-								// set message to chat
-								setMessages((prevMessages) => [...prevMessages, allmsg[key]]);
-							}
-						}
-					} catch (error) {
-						logout();
-						router.push('/');
-					}
-					setLoading(false);
-				});
+			.then(response => response.json()) 
+			.then(data => {
+				setSocketId(socket.id);
+				setcustomerList(data.result);
+			});
 
 			socket.on('chat:message', (newData) => {
 				let returnData = {
@@ -73,7 +68,31 @@ export default function Page() {
 		  endOfPageRef.current?.scrollIntoView({ behavior: 'smooth' });
 		}
 		setLoading(true);
-	  }, [loading]);
+	}, [loading]);
+
+	const getCustomerChat = (thisEmail) => {
+		setCallEmail(thisEmail);
+		fetch("http://localhost:8081/chat/previous_cus?email="+thisEmail+"&socketid="+socketId, {
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${token}`
+			},
+			cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+			redirect: 'follow', // manual, *follow, error
+			referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+		})
+		.then(response => response.json()) 
+		.then(data => {
+			setChatId(data.chatId);
+			const allmsg = data.chatMessage;
+			setMessages('');
+			for (const key in allmsg) {
+				// set message to chat
+				setMessages((prevMessages) => [...prevMessages, allmsg[key]]);
+			}
+			setLoading(false);
+		});
+	}
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
@@ -82,7 +101,9 @@ export default function Page() {
 			// socket.emit('chat:message', { message });
 			if(message!=""){
 				let formData = new FormData();
-				formData.append('email', email);
+				console.log(callEmail);
+				console.log(chatId);
+				formData.append('email', callEmail);
 				formData.append('chatId', chatId);
 				formData.append('message', message);
 				formData.append('time', new Date().toLocaleString());
@@ -113,14 +134,17 @@ export default function Page() {
 	return (
 		<div>
 			<div className="main-container">
-                <div class="customer-container">
-                    <ul class="customer-list" id="customer-list">
+                <div className="customer-container">
+                    <ul className="customer-list" id="customer-list">
                         {customerList.map((data, index) => (
-                            <li key={index}></li>
+                            <li key={index} className="customer-list-item" data-email={data.email}  onClick={() => getCustomerChat(data.email)}>
+								Chat with {data.name}<br />
+								Created At : {converstUTC(data.datetime)}
+							</li>
                         ))}
                     </ul>
                 </div>
-                <div class="chat-container">
+                <div className="chat-container">
                     <button id="logout-button"  onClick={logoutClick}>Logout</button>
                     <h2 className="page-title">Chat With US</h2>
                     <div className="chat-messages">
