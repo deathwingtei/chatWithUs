e960ec75192e0e86d9b670f90489f9d256552c6a
KP <p.kittichet@gmail.com>
2026-07-25 22:25:55 +0700

    update docker file prod

--- server/services/lineNotification.ts
+++ server/services/lineNotification.ts
@@ -1,27 +1,27 @@
-import axios from 'axios';
-import dotenv from 'dotenv';
+import axios from "axios";
+import dotenv from "dotenv";
 dotenv.config();
 
 async function sendNotification(message: string): Promise<any> {
-    const token = process.env.LINE_NOTIFY_TOKEN;
-    const url = "https://notify-api.line.me/api/notify";
+  const token = process.env.LINE_NOTIFY_TOKEN;
+  const url = "https://notify-api.line.me/api/notify";
 
-    const headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': `Bearer ${token}`
-    };
+  const headers = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    Authorization: `Bearer ${token}`,
+  };
 
-    const data = new URLSearchParams();
-    data.append('message', message);
+  const data = new URLSearchParams();
+  data.append("message", message);
 
-    try {
-        const response = await axios.post(url, data, { headers });
-        const result = response.data;
-        return result;
-    } catch (error: any) {
-        console.error("error:", error.message);
-        throw error;
-    }
+  try {
+    const response = await axios.post(url, data, { headers });
+    const result = response.data;
+    return result;
+  } catch (error: any) {
+    console.error("error:", error.message);
+    throw error;
+  }
 }
 
 export default sendNotification;
--- server/services/mailer.ts
+++ server/services/mailer.ts
@@ -1,31 +1,35 @@
-import nodemailer from 'nodemailer';
-import dotenv from 'dotenv';
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.GMAIL_USERNAME,
-        pass: process.env.GMAIL_PASSWORD
-    }
+  service: "gmail",
+  auth: {
+    user: process.env.GMAIL_USERNAME,
+    pass: process.env.GMAIL_PASSWORD,
+  },
 });
 
-async function sendEmail(to: string | string[], subject: string, text: string): Promise<any> {
-    const recipientList = Array.isArray(to) ? to : [to];
-    const mailOptions = {
-        from: process.env.GMAIL_USERNAME,
-        to: recipientList.join(', '),
-        subject,
-        text
-    };
+async function sendEmail(
+  to: string | string[],
+  subject: string,
+  text: string,
+): Promise<any> {
+  const recipientList = Array.isArray(to) ? to : [to];
+  const mailOptions = {
+    from: process.env.GMAIL_USERNAME,
+    to: recipientList.join(", "),
+    subject,
+    text,
+  };
 
-    try {
-        let info = await transporter.sendMail(mailOptions);
-        return info;
-    } catch (error) {
-        console.error('Error sending email:', error);
-        throw error;
-    }
+  try {
+    let info = await transporter.sendMail(mailOptions);
+    return info;
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw error;
+  }
 }
 
 export default sendEmail;
